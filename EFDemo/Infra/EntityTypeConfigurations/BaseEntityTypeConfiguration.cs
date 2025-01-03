b07746e0d3775e6c40887433650422976888cc96
Selcuk ARSLAN <45368072+slckarslan93@users.noreply.github.com>
2025-01-03 22:08:00 +0300

    Add BaseEntityTypeConfiguration and update EF packages
    Removed Infra\EntityTypeConfigurations folder from project.
    Added BaseEntityTypeConfiguration<TEntity> class to configure
    entity types with primary key and date columns. Updated EF
    packages to Microsoft.EntityFrameworkCore 9.0.0 and
    Microsoft.EntityFrameworkCore.Relational 8.0.11.

---
+++ EFDemo/Infra/EntityTypeConfigurations/BaseEntityTypeConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using EFDemo.Infra.Entities.Base;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFDemo.Infra.EntityTypeConfigurations
+{
+    public class BaseEntityTypeConfiguration<TEntity> : IEntityTypeConfiguration<TEntity> where TEntity : BaseEntity
+    {
+        public void Configure(EntityTypeBuilder<TEntity> builder)
+        {
+            builder.HasKey(e => e.Id);
+            builder.Property(e => e.CreatedDate).HasColumnType("datetime2");
+            builder.Property(e => e.ModifiedDate).HasColumnType("datetime2").IsRequired(false);
+        }
+    }
+}
