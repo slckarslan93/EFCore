a63102295241adeea00a59e7ced81bd2600cd453
Selcuk ARSLAN <45368072+slckarslan93@users.noreply.github.com>
2025-01-04 15:17:33 +0300

    Add MovieDbContext and update project configuration
    Updated EFDemo.csproj to remove unused folders and add package references for Microsoft.Extensions.Configuration (8.0.0) and Microsoft.Extensions.Configuration.Json (8.0.1). Added appsettings.json with SQL Server connection string. Introduced MovieDbContext class in MovieDbContext.cs with DbSet properties for MovieEntity, DirectorEntity, GenreEntity, and ActorEntity. Configured database connection and model creation using appsettings.json.

---
+++ EFDemo/Infra/Context/MovieDbContext.cs
@@ -0,0 +1,31 @@
+﻿using EFDemo.Infra.Entities;
+using EFDemo.Infra.EntityTypeConfigurations;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace EFDemo.Infra.Context
+{
+    public class MovieDbContext(DbContextOptions options) : DbContext(options)
+    {
+        public DbSet<MovieEntity> Movies { get; set; }
+        public DbSet<DirectorEntity> Directors { get; set; }
+        public DbSet<GenreEntity> Genres { get; set; }
+        public DbSet<ActorEntity> Actors { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasDefaultSchema("ef");
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(MovieEntityConfiguration).Assembly);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (optionsBuilder.IsConfigured)
+                return;
+
+            var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+            var connstr = configuration.GetConnectionString("SqlServer");
+
+        }
+    }
+}
