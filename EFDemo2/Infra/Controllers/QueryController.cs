3a9604caa6f6543323eb6c4a831833f2892aa0da
Selcuk ARSLAN <45368072+slckarslan93@users.noreply.github.com>
2025-01-07 21:42:12 +0300

    Add QueryController for actor queries in MovieDbContext
    Introduce QueryController class in EFDemo2.Infra.Controllers to handle
    database queries related to actors. Add necessary using directives.
    Implement GetActors method to filter and project actor data into
    ActorViewModel. Include console output for retrieved actor data.
    Define ActorViewModel inner class with Id, FullName, and MovieCount
    properties.

---
+++ EFDemo2/Infra/Controllers/QueryController.cs
@@ -0,0 +1,53 @@
+﻿using EFDemo2.Infra.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFDemo2.Infra.Controllers
+{
+    public class QueryController
+    {
+        private readonly MovieDbContext dbContext;
+
+        public QueryController(MovieDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public async Task GetActors()
+        {
+            // Deferred Execution
+            var query = dbContext.Actors
+                              .Where(a => a.FirstName.Contains("A"));
+
+            //int actorCount = query.Count();
+
+            query = query.Where(a => a.LastName.EndsWith("U"));
+
+            var data = query
+                .Select(a => new ActorViewModel()
+                {
+                    Id = a.Id,
+                    FullName = a.FirstName + " " + a.LastName
+                })
+                .ToList();
+
+            foreach (var item in data)
+            {
+                Console.WriteLine("Data: {0} - {1}", item.Id, item.FullName);
+            }
+
+        }
+
+        public class ActorViewModel
+        {
+            public Guid Id { get; set; }
+
+            public string FullName { get; set; }
+
+            public int MovieCount { get; set; }
+        }
+    }
+}
